18b95ba6276165eef5ff2a674c3952a95551e754
ErwanEL <leblois.erwan@hotmail.fr>
2018-01-06 13:29:39 -0500

    [ADDED] icons [ADDED] market button list [UPDATED] pricevariation function [UPDATED] list order [UPDATED] chart implementation on click

--- app1.js
+++ app1.js
@@ -1,33 +1,21 @@
 const dataVegetales = [
     {
-        "nombre": "Zanahoria",
-        "icon": "icons/carrot.svg",
-        "fechas": ["161117","291117","081217","131217","201217"],
-        "precioskg": ["1290", "1580", "1580", "1780", "1190"]  
-    },
-    {
-        "nombre": "Fresa",
-        "icon": "icons/strawberry.svg",
-        "fechas": ["161117"],
-        "precioskg": ["8760"]  
-    },
-    {
-        "nombre": "Palta Hass",
-        "icon": "icons/avocado.svg",
-        "fechas": ["221217"],
-        "precioskg": ["4180"]  
-    },
-    {
-        "nombre": "Kiwi",
-        "icon": "icons/kiwi.svg",
-        "fechas": ["281117"],
-        "precioskg": ["13180"]  
-    },
-    {
         "nombre": "Banano",
         "icon": "icons/banana.svg",
         "fechas": ["281117","081217","131217","201217","271217"],
         "precioskg": ["1980","1780", "1780", "1780", "1780"]
+    },
+    {
+        "nombre": "Brocoli",
+        "icon": "icons/broccoli.svg",
+        "fechas": ["291117","081217","131217","201217"],
+        "precioskg": ["2980","2980","2980","2980"]  
+    },
+    {
+        "nombre": "Cebolla",
+        "icon": "icons/onion-1.svg",
+        "fechas": ["081217","201217"],
+        "precioskg": ["3200","3190"]  
     },
     {
         "nombre": "Durasno Venezolano",
@@ -36,10 +24,58 @@ const dataVegetales = [
         "precioskg": ["800"]
     },
     {
-        "nombre": "Brocoli",
-        "icon": "icons/broccoli.svg",
-        "fechas": ["291117","081217","131217","201217"],
-        "precioskg": ["2980","2980","2980","2980"]  
+        "nombre": "Espinaca",
+        "icon": "icons/spinach.svg",
+        "fechas": ["131217","271217"],
+        "precioskg": ["5780","5780"]  
+    },  
+    {
+        "nombre": "Fresa",
+        "icon": "icons/strawberry.svg",
+        "fechas": ["161117"],
+        "precioskg": ["8760"]  
+    },
+    {
+        "nombre": "Kiwi",
+        "icon": "icons/kiwi.svg",
+        "fechas": ["281117"],
+        "precioskg": ["13180"]  
+    },
+    {
+        "nombre": "Lechuga Batavia",
+        "icon": "icons/salad-1.svg",
+        "fechas": ["081217","131217","271217"],
+        "preciosUnd": ["1780","1750","1890"]  
+    },    
+    {
+        "nombre": "Palta Hass",
+        "icon": "icons/avocado.svg",
+        "fechas": ["221217"],
+        "precioskg": ["4180"]  
+    },
+    {
+        "nombre": "Papa Capira",
+        "icon": "icons/potatoes-2.svg",
+        "fechas": ["201217"],
+        "precioskg": ["1680"]  
+    },
+    {
+        "nombre": "Papa Nevada",
+        "icon": "icons/potatoes-2.svg",
+        "fechas": ["131217"],
+        "precioskg": ["2980"]  
+    },
+    {
+        "nombre": "Pepino",
+        "icon": "icons/cucumber.svg",
+        "fechas": ["201217"],
+        "precioskg": ["1580"]  
+    },
+    {
+        "nombre": "Pimenton",
+        "icon": "icons/pepper.svg",
+        "fechas": ["081217","131217","201217"],
+        "precioskg": ["4680","4680","4800"]  
     },
     {
         "nombre": "Piña Oro Miel",
@@ -49,28 +85,16 @@ const dataVegetales = [
     },
     {
         "nombre": "Platano Maduro",
-        "icon": "icons/banana.svg",
+        "icon": "icons/platano.svg",
         "fechas": ["011217"],
         "precioskg": ["1980"]  
     },
     {
         "nombre": "Platano Verde",
-        "icon": "icons/banana.svg",
+        "icon": "icons/platano-verde.svg",
         "fechas": ["011217","201217"],
         "precioskg": ["1780","1450"]  
-    },
-    {
-        "nombre": "Pimenton",
-        "icon": "icons/pepper.svg",
-        "fechas": ["081217","131217","201217"],
-        "precioskg": ["4680","4680","4800"]  
-    },
-    {
-        "nombre": "Cebolla",
-        "icon": "icons/onion-1.svg",
-        "fechas": ["081217","201217"],
-        "precioskg": ["3200","3190"]  
-    },
+    }, 
     {
         "nombre": "Repollo",
         "icon": "icons/cabbage.svg",
@@ -78,34 +102,10 @@ const dataVegetales = [
         "precioskg": ["1180"]  
     },
     {
-        "nombre": "Lechuga Batavia",
-        "icon": "icons/salad-1.svg",
-        "fechas": ["081217","131217","271217"],
-        "preciosUnd": ["1780","1750","1890"]  
-    },
-    {
-        "nombre": "Espinaca",
-        "icon": "icons/spinach.svg",
-        "fechas": ["131217","271217"],
-        "precioskg": ["5780","5780"]  
-    },
-    {
-        "nombre": "Papa Nevada",
-        "icon": "icons/potatoes-2.svg",
-        "fechas": ["131217"],
-        "precioskg": ["2980"]  
-    },
-    {
-        "nombre": "Papa Capira",
-        "icon": "icons/potatoes-2.svg",
-        "fechas": ["201217"],
-        "precioskg": ["1680"]  
-    },
-    {
-        "nombre": "Pepino",
-        "icon": "icons/cucumber.svg",
-        "fechas": ["201217"],
-        "precioskg": ["1580"]  
+        "nombre": "Zanahoria",
+        "icon": "icons/carrot.svg",
+        "fechas": ["161117","291117","081217","131217","201217"],
+        "precioskg": ["1290", "1580", "1580", "1780", "1190"]  
     }
 ];
 
@@ -125,6 +125,9 @@ const priceVariation = (e) => {
         variation = `+${precios[precios.length - 1] - precios[precios.length - 2]}COP`
     } else {
         variation = `${precios[precios.length - 1] - precios[precios.length - 2]}COP`;
+    } 
+    if (variation === "0COP" || variation === "NaNCOP") {
+        variation = `+${precios[precios.length - 1]}COP`;
     }
     return variation;
 }
@@ -159,8 +162,10 @@ listElements.forEach(e => {
 //permet d'implementer la courbe pour chaque click
 
 listElements.forEach(e => e.addEventListener('click', () => {  
+    listDivs.forEach(div => div.style.display = 'none'); //fait disparaitre l'element precedemment clické
     dataVegetales.forEach(data => {
         if(e.nextElementSibling.id === getId(data.nombre)) {
+            e.nextElementSibling.style.display = 'block';
             new Chartist.Line(`#${e.nextElementSibling.id}`, { labels: data.fechas,
                                                 series: [
                                                     data.precioskg || data.preciosUnd
@@ -168,22 +173,7 @@ listElements.forEach(e => e.addEventListener('click', () => {
                                             });
         }
     })
-}))
-
-//permet d'implementer la courbe dynamiquement
-
-// listDivs.forEach(div => {
-//     dataVegetales.forEach(data => {
-//         if(div.id === getId(data.nombre)) {
-//             new Chartist.Line(`#${div.id}`, { labels: data.fechas,
-//                                                 series: [
-//                                                     data.precioskg || data.preciosUnd
-//                                                 ]
-//                                             });
-//         }
-//     })
-// })
-
+}));
 
 //modèle
 //let dataChart = {
