577bbcb4b26555d4975ce99677415b980b85e2bf
ErwanEL <leblois.erwan@hotmail.fr>
2018-01-09 14:46:47 -0500

    [FIXED]:  +- display pills [ADDED]: styles.css, veges [UPDATED] data

--- app1.js
+++ app1.js
@@ -1,116 +1,128 @@
 const dataVegetales = [
     {
         "nombre": "Banano",
-        "icon": "icons/banana.svg",
-        "fechas": ["281117","081217","131217","201217","271217"],
+        "icon": "banana",
+        "fechas": ["291117","081217","131217","201217","020118"],
         "precioskg": ["1980","1780", "1780", "1780", "1780"]
     },
     {
         "nombre": "Brocoli",
-        "icon": "icons/broccoli.svg",
+        "icon": "broccoli",
         "fechas": ["291117","081217","131217","201217","020118"],
-        "precioskg": ["2980","2980","2980","2980","2980"]  
+        "precioskg": ["2980","2980","2980","2980","2980"]
     },
     {
         "nombre": "Cebolla",
-        "icon": "icons/onion-1.svg",
+        "icon": "onion-1",
         "fechas": ["081217","201217","020118"],
         "precioskg": ["3200","3190","3200"]  
     },
     {
+        "nombre": "Coliflor",
+        "icon": "cauliflower",
+        "fechas": ["0"],
+        "precioskg": ["4500"]  
+    },
+    {
+        "nombre": "Calabacin Verde",
+        "icon": "courgette",
+        "fechas": ["0"],
+        "precioskg": ["2990"]
+    },
+    {
         "nombre": "Durasno Venezolano",
-        "icon": "icons/peach.svg",
+        "icon": "peach",
         "fechas": ["291117"],
         "precioskg": ["800"]
     },
     {
         "nombre": "Espinaca",
-        "icon": "icons/spinach.svg",
+        "icon": "spinach",
         "fechas": ["131217","271217"],
         "precioskg": ["5780","5780"]  
     },  
     {
         "nombre": "Fresa",
-        "icon": "icons/strawberry.svg",
+        "icon": "strawberry",
         "fechas": ["161117"],
         "precioskg": ["8760"]  
     },
     {
         "nombre": "Kiwi",
-        "icon": "icons/kiwi.svg",
+        "icon": "kiwi",
         "fechas": ["281117"],
         "precioskg": ["13180"]  
     },
     {
         "nombre": "Lechuga Batavia",
-        "icon": "icons/salad-1.svg",
+        "icon": "salad-1",
         "fechas": ["081217","131217","271217"],
         "preciosUnd": ["1780","1750","1890"]  
     },    
     {
         "nombre": "Palta Hass",
-        "icon": "icons/avocado.svg",
+        "icon": "avocado",
         "fechas": ["221217","050118"],
         "precioskg": ["4180","3880"]  
     },
     {
         "nombre": "Papa Capira",
-        "icon": "icons/potatoes-2.svg",
+        "icon": "potatoes-2",
         "fechas": ["201217"],
         "precioskg": ["1680"]  
     },
     {
         "nombre": "Papa Nevada",
-        "icon": "icons/potatoes-2.svg",
+        "icon": "potatoes-2",
         "fechas": ["131217"],
         "precioskg": ["2980"]  
     },
     {
         "nombre": "Pepino",
-        "icon": "icons/cucumber.svg",
+        "icon": "cucumber",
         "fechas": ["201217","050118"],
         "precioskg": ["1580","1490"]  
     },
     {
         "nombre": "Pimenton",
-        "icon": "icons/pepper.svg",
+        "icon": "pepper",
         "fechas": ["081217","131217","201217","020118"],
         "precioskg": ["4680","4680","4800","5690"]  
     },
     {
         "nombre": "Piña Oro Miel",
-        "icon": "icons/pineapple.svg",
+        "icon": "pineapple",
         "fechas": ["291117","081217","271217"],
         "precioskg": ["1980","1780","2180"]  
     },
     {
         "nombre": "Platano Maduro",
-        "icon": "icons/platano.svg",
+        "icon": "platano",
         "fechas": ["011217","050118"],
         "precioskg": ["1980","980"]  
     },
     {
         "nombre": "Platano Verde",
-        "icon": "icons/platano-verde.svg",
+        "icon": "platano-verde",
         "fechas": ["011217","201217","050118"],
         "precioskg": ["1780","1450","1780"]  
     }, 
     {
         "nombre": "Repollo",
-        "icon": "icons/cabbage.svg",
+        "icon": "cabbage",
         "fechas": ["081217"],
         "precioskg": ["1180"]  
     },
     {
         "nombre": "Zanahoria",
-        "icon": "icons/carrot.svg",
+        "icon": "carrot",
         "fechas": ["161117","291117","081217","131217","201217","050118"],
         "precioskg": ["1290", "1580", "1580", "1780", "1190","1380"]  
     }
 ];
 
 //permet de creer un id valide pour les divs
-function getId (a) {
+function getId(a) {
     return a.split(' ').join('').toLowerCase();
 }
 
@@ -121,7 +133,7 @@ const listGroup = document.querySelector('.list-group');
 const priceVariation = (e) => {
     let precios = e.precioskg || e.preciosUnd;
     let variation;
-    if (precios[precios.length - 1] > precios[precios.length - 2]) {
+    if (Number(precios[precios.length - 1]) > Number(precios[precios.length - 2])) {
         variation = `+${precios[precios.length - 1] - precios[precios.length - 2]}COP`
     } else {
         variation = `${precios[precios.length - 1] - precios[precios.length - 2]}COP`;
@@ -136,7 +148,7 @@ const priceVariation = (e) => {
 const list = () =>  {
     dataVegetales.forEach(data => {
         listGroup.innerHTML += `<li class="list-group-item d-flex justify-content-between align-items-center">
-                                <img src=${data.icon}>
+                                <img src=icons/${data.icon}.svg>
                                 ${data.nombre}
                                 <span class="badge badge-danger badge-pill">${priceVariation(data)}</span>
                                 </li>
@@ -174,6 +186,23 @@ listElements.forEach(e => e.addEventListener('click', () => {
         }
     })
 }));
+
+//permet d'afficher toutes les courbes
+// listElements.forEach(e => e.addEventListener('click', () => {  
+//     listDivs.forEach(div => div.style.display = 'none'); //fait disparaitre l'element precedemment clické
+//     dataVegetales.forEach(data => {
+//         if(e.nextElementSibling.id === getId(data.nombre)) {
+//             e.nextElementSibling.style.display = 'block';
+//             new Chartist.Line(`#${e.nextElementSibling.id}`, { labels: data.fechas,
+//                                                 series: [
+//                                                     data.precioskg.consumo,
+//                                                     data.precioskg.jumbo,
+//                                                     data.precioskg.exitoExpress
+//                                                 ]
+//                                             });
+//         }
+//     })
+// }));
 
 //modèle
 //let dataChart = {
