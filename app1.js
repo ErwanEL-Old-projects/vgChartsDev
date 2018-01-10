faf50ac4688e243222793b663a38551b0ffac67e
ErwanEL <leblois.erwan@hotmail.fr>
2018-01-10 14:48:16 -0500

    [ADDED] data, exito jumbo charts [UPDATED] click function

--- app1.js
+++ app1.js
@@ -3,121 +3,175 @@ const dataVegetales = [
         "nombre": "Banano",
         "icon": "banana",
         "fechas": ["291117","081217","131217","201217","020118"],
-        "precioskg": ["1980","1780", "1780", "1780", "1780"]
+        "precios": {
+            "consumo" :["1980","1780", "1780", "1780", "1780"],
+            "jumbo" :   ["1190","1190","1190","1190","1190"], //exemple
+            "exito" : ["1290","1290","1290","1290","2000"] //exemple
+        } 
     },
     {
         "nombre": "Brocoli",
         "icon": "broccoli",
         "fechas": ["291117","081217","131217","201217","020118"],
-        "precioskg": ["2980","2980","2980","2980","2980"]
+        "precios": {
+            "consumo" : ["2980","2980","2980","2980","2980"]
+        }
     },
     {
         "nombre": "Cebolla",
         "icon": "onion-1",
         "fechas": ["081217","201217","020118"],
-        "precioskg": ["3200","3190","3200"]  
+        "precios": {
+            "consumo" :["3200","3190","3200"],
+            "jumbo" : [],
+            "exito" : ["2700"]
+        }
     },
     {
         "nombre": "Coliflor",
         "icon": "cauliflower",
         "fechas": ["0"],
-        "precioskg": ["4500"]  
+        "precios": {
+            "consumo" :["4500"],
+            "jumbo" : ["4500"],
+            "exito" : []
+        }
     },
     {
         "nombre": "Calabacin Verde",
         "icon": "courgette",
         "fechas": ["0"],
-        "precioskg": ["2990"]
+        "precios": {
+            "consumo" :[],
+            "jumbo" : ["2990"],
+            "exito" : []
+        }
     },
     {
         "nombre": "Durasno Venezolano",
         "icon": "peach",
         "fechas": ["291117"],
-        "precioskg": ["800"]
+        "precios": {
+            "consumo" :["800"]
+        }
     },
     {
         "nombre": "Espinaca",
         "icon": "spinach",
         "fechas": ["131217","271217"],
-        "precioskg": ["5780","5780"]  
+        "precios": {
+            "consumo" :["5780","5780"]
+        }
     },  
     {
         "nombre": "Fresa",
         "icon": "strawberry",
         "fechas": ["161117"],
-        "precioskg": ["8760"]  
+        "precios": {
+            "consumo" :["8760"]
+        }
     },
     {
         "nombre": "Kiwi",
         "icon": "kiwi",
         "fechas": ["281117"],
-        "precioskg": ["13180"]  
+        "precios": {
+            "consumo" :["13180"]
+        } 
     },
     {
         "nombre": "Lechuga Batavia",
         "icon": "salad-1",
         "fechas": ["081217","131217","271217"],
-        "preciosUnd": ["1780","1750","1890"]  
+        "precios": {
+            "consumo" :["1780","1750","1890"]
+        }
     },    
     {
         "nombre": "Palta Hass",
         "icon": "avocado",
         "fechas": ["221217","050118"],
-        "precioskg": ["4180","3880"]  
+        "precios": {
+            "consumo" : ["4180","3880"]
+        }
     },
     {
         "nombre": "Papa Capira",
         "icon": "potatoes-2",
         "fechas": ["201217"],
-        "precioskg": ["1680"]  
+        "precios": {
+            "consumo" :["1680"]
+        }
     },
     {
         "nombre": "Papa Nevada",
         "icon": "potatoes-2",
         "fechas": ["131217"],
-        "precioskg": ["2980"]  
+        "precios": {
+            "consumo" :["2980"]
+        }
     },
     {
         "nombre": "Pepino",
         "icon": "cucumber",
         "fechas": ["201217","050118"],
-        "precioskg": ["1580","1490"]  
+        "precios": {
+            "consumo" :["1580","1490"],
+            "jumbo" : ["2290"],
+            "exito" : []
+        }
     },
     {
         "nombre": "Pimenton",
         "icon": "pepper",
         "fechas": ["081217","131217","201217","020118"],
-        "precioskg": ["4680","4680","4800","5690"]  
+        "precios": {
+            "consumo" :["4680","4680","4800","5690"]  
+        } 
     },
     {
         "nombre": "Piña Oro Miel",
         "icon": "pineapple",
         "fechas": ["291117","081217","271217"],
-        "precioskg": ["1980","1780","2180"]  
+        "precios": {
+            "consumo" :["1980","1780","2180"] 
+        }
     },
     {
         "nombre": "Platano Maduro",
         "icon": "platano",
         "fechas": ["011217","050118"],
-        "precioskg": ["1980","980"]  
+        "precios": {
+            "consumo" :["1980","980"] 
+        }
     },
     {
         "nombre": "Platano Verde",
         "icon": "platano-verde",
         "fechas": ["011217","201217","050118"],
-        "precioskg": ["1780","1450","1780"]  
+        "precios": {
+            "consumo" :["1780","1450","1780"] 
+        }
     }, 
     {
         "nombre": "Repollo",
         "icon": "cabbage",
         "fechas": ["081217"],
-        "precioskg": ["1180"]  
+        "precios": {
+            "consumo" :["1180"],
+            "jumbo" : ["1900"],
+            "exito" : []
+        }
     },
     {
         "nombre": "Zanahoria",
         "icon": "carrot",
         "fechas": ["161117","291117","081217","131217","201217","050118"],
-        "precioskg": ["1290", "1580", "1580", "1780", "1190","1380"]  
+        "precios": {
+            "consumo" :["1290", "1580", "1580", "1780", "1190","1380"],
+            "jumbo" : [],
+            "exito" : ["2000"]
+        }
     }
 ];
 
@@ -131,7 +185,7 @@ const listGroup = document.querySelector('.list-group');
 
 //calcul la variation du prix a integrer dans les pills
 const priceVariation = (e) => {
-    let precios = e.precioskg || e.preciosUnd;
+    let precios = e.precios.consumo;
     let variation;
     if (Number(precios[precios.length - 1]) > Number(precios[precios.length - 2])) {
         variation = `+${precios[precios.length - 1] - precios[precios.length - 2]}COP`
@@ -170,24 +224,24 @@ listElements.forEach(e => {
     };
 });
 
-
-//permet d'implementer la courbe pour chaque click
-
+//permet d'afficher toutes les courbes
 listElements.forEach(e => e.addEventListener('click', () => {  
     listDivs.forEach(div => div.style.display = 'none'); //fait disparaitre l'element precedemment clické
     dataVegetales.forEach(data => {
         if(e.nextElementSibling.id === getId(data.nombre)) {
             e.nextElementSibling.style.display = 'block';
             new Chartist.Line(`#${e.nextElementSibling.id}`, { labels: data.fechas,
-                                                series: [
-                                                    data.precioskg || data.preciosUnd
-                                                ]
-                                            });
+                series: [
+                    data.precios.consumo,
+                    data.precios.jumbo,
+                    data.precios.exito
+                ]
+            });
         }
     })
 }));
 
-//permet d'afficher toutes les courbes
+//permet d'implementer la courbe pour chaque click (ancienne version)
 // listElements.forEach(e => e.addEventListener('click', () => {  
 //     listDivs.forEach(div => div.style.display = 'none'); //fait disparaitre l'element precedemment clické
 //     dataVegetales.forEach(data => {
@@ -195,9 +249,7 @@ listElements.forEach(e => e.addEventListener('click', () => {
 //             e.nextElementSibling.style.display = 'block';
 //             new Chartist.Line(`#${e.nextElementSibling.id}`, { labels: data.fechas,
 //                                                 series: [
-//                                                     data.precioskg.consumo,
-//                                                     data.precioskg.jumbo,
-//                                                     data.precioskg.exitoExpress
+//                                                     data.precioskg || data.preciosUnd
 //                                                 ]
 //                                             });
 //         }
